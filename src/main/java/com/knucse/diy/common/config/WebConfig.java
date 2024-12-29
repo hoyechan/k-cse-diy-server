28fec1189f6a9795ebaa2855f0500971d680ee45
yechanho <hoyechan04@knu.ac.kr>
2024-12-29 23:37:13 +0900

    feat: CORS설정

---
+++ src/main/java/com/knucse/diy/common/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.knucse.diy.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 엔드포인트에 대해
+                .allowedOrigins("http://localhost:3000") // 프론트엔드 주소 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // 허용할 HTTP 메서드
+    }
+}
+
