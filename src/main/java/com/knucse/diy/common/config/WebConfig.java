16fd8b9f8aa200597b64b90ac457d31f1841607f
yechanho <hoyechan04@knu.ac.kr>
2025-01-10 11:25:35 +0900

    CORS 설정 PATCH 추가

--- src/main/java/com/knucse/diy/common/config/WebConfig.java
+++ src/main/java/com/knucse/diy/common/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해
-                .allowedOrigins("http://localhost:3000") // 프론트엔드 주소 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE"); // 허용할 HTTP 메서드
+                .allowedOrigins("http://localhost:3000")// 프론트엔드 주소 허용
+                .allowedMethods("GET", "POST", "PUT","PATCH","DELETE"); // 허용할 HTTP 메서드
     }
 }
 
