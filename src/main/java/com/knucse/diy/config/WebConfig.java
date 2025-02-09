aaa2c07f197974dc9d6c892b85d36d57180a8055
yechanho <hoyechan04@knu.ac.kr>
2025-02-09 16:50:56 +0900

    webconfig 수정

--- src/main/java/com/knucse/diy/config/WebConfig.java
+++ src/main/java/com/knucse/diy/config/WebConfig.java
@@ -11,9 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해
-                .allowedOrigins("http://localhost:3000")// 프론트엔드 주소 허용
-                .allowedOrigins("https://diy.knucse.site")
-                .allowedMethods("GET", "POST", "PUT","PATCH","DELETE"); // 허용할 HTTP 메서드
+                .allowedOrigins("http://localhost:3000","https://diy.knucse.site")
+                .allowedMethods("GET", "POST", "PUT","PATCH","DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
 
